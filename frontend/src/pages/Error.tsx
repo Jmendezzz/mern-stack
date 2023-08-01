38b649d68dfb8c5dc238364d3c5c293f39794e9a
Juan Gerardo <jmendez1025@cue.edu.co>
2023-07-31 22:26:20 -0500

    feat(errors): 404 Error page implemented

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,15 +1,20 @@
 import { Link, useRouteError } from "react-router-dom";
+import error_image from  '../assets/images/errors/not-found.svg';
 type RouteError = {
     status: string
 }
 export const Error  = () =>{
     const error:RouteError = useRouteError();
+    console.log(error.status);
 
-    return(
-        <div>
-            <h1>Error page</h1>
-            <h2>{error.status}</h2>
-            <Link to={'/'} >back home</Link>
-        </div>
-    );
+    if(error.status == '404'){
+        return(
+            <div className="flex flex-col w-full items-center justify-center h-full gap-5">
+                <h1>Page not found</h1>
+                <img src={error_image} alt="Error image" />
+                <Link to={'/'} className="btn p-5" >Back home</Link>
+            </div>
+        );
+
+    }
 }
